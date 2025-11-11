7268a15249e10d5ba568441c9c01f63dde3282ce
saad7410 <69268075+saad7410@users.noreply.github.com>
2025-11-11 17:41:15 +0100

    progress

--- verification_scripts/key_check.py
+++
@@ -1,6 +0,0 @@
-from netCDF4 import Dataset
-ds = Dataset("data/A03_53310/A03_butene_0p50fs_dynamic.nc")
-print("VARS:", list(ds.variables.keys()))
-print("GLOBAL ATTRS:", ds.ncattrs())
-for k,v in ds.variables.items():
-    print(k, v.shape, v.dtype, v.ncattrs())
